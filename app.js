b7ee5c8f1eed02862b3dcef0a1c273918295218c
Faizanansari222 <faizanrazaansari07@gmail.com>
2023-10-21 20:33:57 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// alert('happy code')
+
+// var userAge = confirm ('i am 14 years old')
+// console.log (userAge)
+// let sum = prompt("Sum of 13 and 9 is equal to");
+// console.log(sum);
+
+// console.log(13*9);
+// console.log(13/1.4444444);
+// let = prompt ('Result of dividing 13 and 9 is ')
+// console.log (13/9);
+// let a=2,b=1;
+// var result=(--a - --b + ++b + b--);
+// console.log(result);
+
+// result = (--a ) ;
+// console.log(result);
+
+// result=(--a - --b);
+// console.log(result);
+
+// var result=(--a - --b + ++b);
+// console.log(result);
+
+
+
+
+// let name = "Faizan" ;
+// let age = "28";
+// console.log("my name is" + ' ' + name + ' ' + "and my age is" + ' ' + age);
+
+
+// let userName = prompt('Your First Name')
+// let chemMarks = Number ( prompt('Enter Chemistry marks'))
+// let mathMarks = Number ( prompt('Enter Maths marks'))
+// let bioMarks = Number ( prompt('Enter Biology marks'))
+// let phyMarks = Number ( prompt('Enter Physics marks'))
+
+
+
+
+
+let totalMarks = 400;
+let userTotal = chemMarks + mathMarks + bioMarks + phyMarks;
+let percentage = userTotal / totalMarks * 100;
+console.log (userName + ' ' + 'has Got'+ ' '+ percentage +'%' )
+
+
+
+// let num = 1;
+// num +=  2;
+// console.log(num);
+
