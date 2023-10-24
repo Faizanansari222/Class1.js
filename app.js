a4c780b8ac5276461a3a89a2ff4cbbd53d74f60e
Faizanansari22 <faizanrazaansari07@gmail.com>
2023-10-24 16:34:05 +0500

    Assigenment 1 js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 
-// alert('happy code')
+// Assignement 01
+// alert('happy code \n Write on my Browser')
 
 // var userAge = confirm ('i am 14 years old')
 // console.log (userAge)
 // let sum = prompt("Sum of 13 and 9 is equal to");
 // console.log(sum);
 
-// console.log(13*9);
-// console.log(13/1.4444444);
-// let = prompt ('Result of dividing 13 and 9 is ')
-// console.log (13/9);
+// var mass = alert("Hello... I can run JS Through my web browser Console")
+//     console.log("Hello... I can run JS Through my web browser Console")
+
+// let mult = +prompt("multiplication of 13 and 9 is equal to");
+// if (mult == 117){
+//     console.log(true, mult, 'is the right answer' )
+// }else{
+//     console.log(false, "the ansawer is " , mult)
+// }
+// 
+// let division =alert (13/1.4444444);
+// let  div=alert (13/9)
+// console.log (mult, division, div);
 // let a=2,b=1;
 // var result=(--a - --b + ++b + b--);
 // console.log(result);
@@ -24,7 +34,7 @@
 // console.log(result);
 
 
-
+//Result Assignment 02 
 
 // let name = "Faizan" ;
 // let age = "28";
@@ -37,14 +47,17 @@
 // let bioMarks = Number ( prompt('Enter Biology marks'))
 // let phyMarks = Number ( prompt('Enter Physics marks'))
 
+// let totalMarks = 400;
+// let userTotal = chemMarks + mathMarks + bioMarks + phyMarks;
+// let percentage = userTotal / totalMarks * 100;
+// console.log (userName + ' ' + 'has Got'+ ' '+ percentage +'%' )
 
+// let massage = alert("Hello Wolrd")
+// let myName = ("Faizan Raza Ansari")
+// let age = +prompt("enter Your Age")
 
+// console.log(myName , age)
 
-
-let totalMarks = 400;
-let userTotal = chemMarks + mathMarks + bioMarks + phyMarks;
-let percentage = userTotal / totalMarks * 100;
-console.log (userName + ' ' + 'has Got'+ ' '+ percentage +'%' )
 
 
 
@@ -52,3 +65,58 @@ console.log (userName + ' ' + 'has Got'+ ' '+ percentage +'%' )
 // num +=  2;
 // console.log(num);
 
+
+
+
+//assigement Percentge with Grade
+
+// var percentage = 40;
+// if (percentage => 80 )  {
+//     console.log( "you Got Grade A+")
+//  } else if (percentage < 80){
+//         console.log("Grade A")
+
+//     }
+//     else if (percentage <= 70){
+//         console.log("Grade B")
+//     }
+//     else if (percentage <= 60){
+//         console.log("Grade C")
+//     }else{
+//     console.log(Fail)
+// }
+
+//Circket Assignment 03 
+
+// let wicket = 9
+// if (wicket <= 3) {
+//     console.log("Pakistan is lost The Match");
+// } else if (wicket > 3 && wicket < 6){
+//     console.log ("Pakistan Won The Match");
+// }else{
+//     console.log("Match Draw")
+// }
+
+
+// New Condation of if else
+
+// var time = +prompt("Put The Time");
+//  if (time >= 7 && time < 12){
+//     console.log("Good Morning");
+//  }else if(time >= 12 && time < 19){
+//     console.log("Good Evening");
+//  }else if (time >= 19 && time <= 24){
+//     console.log("Good Night")
+//  } else if (time >= 00 && time < 7){
+//     console.log("Good Night")
+//  }else;
+
+
+
+
+// let userName = prompt("Enter Your Name")
+// let parantName = prompt("Parant's Name")
+// let location = prompt("Location")
+// let jobTitle = prompt("Job Title")
+
+// console.log('My Name is '+ Name +'My parents Name is '+ parantName +'and i am from ' +location +'i am doing the job of ' + jobTitle);
