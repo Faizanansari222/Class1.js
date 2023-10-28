7491b3fc4a901fb39c5ddac03cc360fd1d93b09a
Faizanansari22 <faizanrazaansari07@gmail.com>
2023-10-28 18:44:13 +0500

    New update 10-2802023

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // Assignement 01
 // alert('happy code \n Write on my Browser')
 
@@ -16,7 +15,7 @@
 // }else{
 //     console.log(false, "the ansawer is " , mult)
 // }
-// 
+//
 // let division =alert (13/1.4444444);
 // let  div=alert (13/9)
 // console.log (mult, division, div);
@@ -33,13 +32,11 @@
 // var result=(--a - --b + ++b);
 // console.log(result);
 
-
-//Result Assignment 02 
+//Result Assignment 02
 
 // let name = "Faizan" ;
 // let age = "28";
 // console.log("my name is" + ' ' + name + ' ' + "and my age is" + ' ' + age);
-
 
 // let userName = prompt('Your First Name')
 // let chemMarks = Number ( prompt('Enter Chemistry marks'))
@@ -58,35 +55,12 @@
 
 // console.log(myName , age)
 
-
-
-
 // let num = 1;
 // num +=  2;
 // console.log(num);
 
 
-
-
-//assigement Percentge with Grade
-
-// var percentage = 40;
-// if (percentage => 80 )  {
-//     console.log( "you Got Grade A+")
-//  } else if (percentage < 80){
-//         console.log("Grade A")
-
-//     }
-//     else if (percentage <= 70){
-//         console.log("Grade B")
-//     }
-//     else if (percentage <= 60){
-//         console.log("Grade C")
-//     }else{
-//     console.log(Fail)
-// }
-
-//Circket Assignment 03 
+//Circket Assignment 03
 
 // let wicket = 9
 // if (wicket <= 3) {
@@ -96,7 +70,6 @@
 // }else{
 //     console.log("Match Draw")
 // }
-
 
 // New Condation of if else
 
@@ -108,15 +81,317 @@
 //  }else if (time >= 19 && time <= 24){
 //     console.log("Good Night")
 //  } else if (time >= 00 && time < 7){
-//     console.log("Good Night")
-//  }else;
+    //     console.log("Good Night")
+    //  }else;
 
-
-
-
-// let userName = prompt("Enter Your Name")
-// let parantName = prompt("Parant's Name")
+    // let userName = prompt("Enter Your Name")
+    // let parantName = prompt("Parant's Name")
 // let location = prompt("Location")
 // let jobTitle = prompt("Job Title")
 
 // console.log('My Name is '+ Name +'My parents Name is '+ parantName +'and i am from ' +location +'i am doing the job of ' + jobTitle);
+
+// function Class Work
+// perameter functin ko banata krta hai
+//
+// //
+
+// function greetUser() {
+    //     console.log('hello word');
+//     console.log('hello word');
+//     console.log('hello word');
+//     console.log('hello word');
+//     console.log('hello word');
+// }
+
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+
+// function greetUser(name,age,salary) {
+    //     console.log('my name is ' + name + ' and my age is ' + age + ' and my salary is '+ salary);
+    
+    // }
+    
+    // greetUser( 'Faizan' , 20 , 20000 )
+    
+    // greetUser( 'rahim' , 23 , 30000 )
+    
+    // greetUser( 'bilal',35,2000  )
+    
+    // function greetUser(){
+        //     console.log('hello user');
+// }
+// console.log(greetUser());
+
+// function greetUser (){
+//     console.log('hello world');
+//     return "hi"
+// }
+// console.log(greetUser());
+
+// let names= prompt("Your Name");
+//     let pro1= prompt("Your first Product");
+//     let pro2= prompt("Your second Product");
+//     let price1= prompt("product Price1");
+//     let price2= prompt("product Price2");
+
+// function greetUser (){
+    
+    //     return "your Name is"+ names+ " your Product is " + pro1 + pro2 +" your total amount is " price1 + price2
+// }
+
+// console.log(greetUser("your Name is"+ names+ " your Product is " + pro1 + pro2 +" your total amount is "price1+ price2))
+
+// Assignment 8-11
+
+// let userName = prompt("enter your Name")
+// function input (){alert ("Hi, " + userName)}
+// input();
+
+// function inputTable (){
+    // let userInput = +prompt("Enter a Number");
+// let serialNumber = 3
+// let num = 5;
+//     return userInput * serialNumber
+// }
+
+// console.log(userInput+ "X"+ serialNumber+"=" + inputTable());
+
+// let cityName = prompt("Enter your city");
+//  if (cityName.toLowerCase() == "karachi"){
+    //     alert("Welcome to city og light");
+    //  }else{
+        //     alert("Welcome to "+cityName);
+        //  }
+        
+        // let gender = prompt("Gender");
+        //  if (gender.toLowerCase()==="male"){
+            //     alert("Good Morning Sir");
+            //  }else if (gender.toLowerCase()==="female") {
+                //     alert("Good Morning Ma'am");
+//  }else{
+//     alert("Good Morning " + gender)
+//  }
+
+// let userInput = prompt("The Trafic light color is");
+// if (userInput.toLowerCase() === "red"){
+//     alert("vehicles must stop")
+// }else if (userInput.toLowerCase() === "yellow") {
+//     alert("vehicles should get ready to move")
+// }else if (userInput.toLowerCase() === "green") {
+//     alert("vehicles can move now");
+// }
+
+
+
+
+
+
+
+
+
+
+// let guestName = prompt("Your Good Name");
+// let crentAge = +prompt("Your Crent age");
+// if (crentAge >= 18) {
+    //   alert("You are Welocme "+ guestName);
+    // }else{
+        //     alert ("Sorry! "+ guestName +" you are not allowed")
+        // }
+
+        
+        
+        
+        
+        
+        
+// var a = 5;
+// if (a === 5){
+    //  alert("given condition for variable a is true");
+    // }else{
+        //     alert ("given condition is false")
+        // }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // let c = 12;
+        
+        // if (++c === 13){
+            //  alert("condition 1 is true");
+// }else{
+//     alert("condition 1 is False")
+// };
+// c = 12;
+
+// if (c === 13){
+    //  alert("condition 2 is true");
+    // }else{
+        //     alert("condition 2 is False")
+        // }
+        // c = 12;
+        
+        // if (++c < 14){
+            //  alert("condition 3 is true");
+            // }else{
+//     alert("condition 3 is False")
+// }
+
+// c = 12;
+
+// if(c === 14){
+    //  alert("condition 4 is true");
+    // }else{
+        //     alert("condition 4 is False")
+        // }
+        
+        
+        
+        
+        //assigement Percentge with Grade
+        
+        // var percentage = 40;
+        // if (percentage => 80 )  {
+        //     console.log( "you Got Grade A+")
+        //  } else if (percentage < 80){
+        //         console.log("Grade A")
+        
+        //     }
+        //     else if (percentage <= 70){
+        //         console.log("Grade B")
+        //     }
+        //     else if (percentage <= 60){
+        //         console.log("Grade C")
+        //     }else{
+        //     console.log(Fail)
+        // }
+        
+
+
+
+        // let a= "Faizan"
+        // let b= 22
+        // let c= true
+
+        // console.log(typeof a);
+        // console.log(typeof b);
+        // console.log(typeof c);
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+// let shirtPrice = 1000
+// let quantityOfShirt = prompt("Quantity of T-Shirt")
+// let totalPriceOfShirt = quantityOfShirt * shirtPrice 
+
+// let usbPrice = 700
+// let totalPriceOfUsb = quantityOfUSB * usbPrice
+
+// let shipCharg = 250
+
+// let totalPrice = (totalPriceOfShirt + totalPriceOfUsb + shipCharg);
+// let discPrice = (totalPrice* 10 / 100);
+// let finalAmo = totalPrice - discPrice;
+// alert("Your Total Amount after Discount is " + finalAmo);
+
+// let quantityOfUSB = prompt("Quantity of USB Flash")
+
+
+
+
+
+
+
+
+// let guessNumber = 17;
+// let userGuess = +prompt("Guess The Number")
+
+// if(userGuess === 17){
+//     alert ("Bingo! Correct Answer");
+
+// }else if (userGuess > 17 && userGuess < 19) {
+//     alert("Close enough to the correct answer")
+// }else if(userGuess < 17 && userGuess > 15){
+//     alert("Close enough to the correct answer")
+// }
+
+// else{
+//     alert("Your Answer is wrong")
+// }
+
+
+
+
+
+
+
+
+// let temp = +prompt("write the Crent Temperature")
+
+// if (temp > 40 ){
+//     alert("It is too hot outside.")
+// }else if (temp >20 && temp <=40){
+//     alert("The Weather today is Normal.")
+// }else if (temp >10 && temp <= 20){
+//     alert("Today Weather is cool.")
+// }
+// else if (temp > 0 && temp <= 10){
+//     alert ("OMG! Today’s weather is so Cool.")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+let firstNumber = +prompt ("Type First Number")
+let operation = prompt ("+, -, *, /, %")
+let secondNumber = +prompt ("Type Second Number")
+if (operation = "-"){
+    alert(firstNumber - secondNumber)
+} else if (operation = "+"){
+    alert(firstNumber + secondNumber)
+}else if (operation = "*"){
+    alert(firstNumber * secondNumber)
+}else if (operation = "/"){
+    alert(firstNumber / secondNumber)
+}else if (operation = "%"){
+    alert(firstNumber % secondNumber)
+}else{
+    alert("Error! Some mistake, Please Type CareFully")
+}
+
+
+
+
+
+
+
